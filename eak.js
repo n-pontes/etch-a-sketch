abde941d7c78c9dcf9c6e125c5e05cd8b59c0bfa
n-pontes <nuno06.np@gmail.com>
2024-09-06 11:23:59 +0200

    Add inside the for loop a class to each square div

--- eak.js
+++ eak.js
@@ -1,8 +1,11 @@
 const squareCont = document.querySelector('.sContainer');
 const newSquare = document.createDocumentFragment();
 
+// Creates 256 divs in a 16x16 grid
+
 for (let i = 0; i < 256; i++) {
     const square = document.createElement('div');
+    square.classList.add('square'); // Add the 'square' class to each div
     newSquare.appendChild(square);
 }
 
