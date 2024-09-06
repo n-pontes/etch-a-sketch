dff962cb063edb1729575ebb801d23decb3e4629
n-pontes <nuno06.np@gmail.com>
2024-09-06 10:49:46 +0200

    Remove unnecessary style for the divs

--- eak.js
+++ eak.js
@@ -1,6 +1,5 @@
 const squareCont = document.querySelector('.sContainer')
 const square = document.createElement('div');
-square.style.width = '16px';
-square.style.height = '16px';
+
 
 squareCont.appendChild(square);
