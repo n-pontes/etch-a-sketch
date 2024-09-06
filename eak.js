f1047f0b0ea0226c8ab92137c2f5350c30ca2cff
n-pontes <nuno06.np@gmail.com>
2024-09-06 10:39:13 +0200

    Reference the div that has the squares inside - Used document.querySelector to select the class of the div - The div as then appended to its parent

--- eak.js
+++ eak.js
@@ -1,4 +1,6 @@
+const squareCont = document.querySelector('sContainer')
 const square = document.createElement('div');
 square.style.width = '16px';
 square.style.height = '16px';
 
+squareCont.appendChild(square);
