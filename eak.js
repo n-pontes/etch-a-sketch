8f85d875a78d2fe70073e2597ff627e23a3dfd70
n-pontes <nuno06.np@gmail.com>
2024-09-06 11:32:59 +0200

    Wrap loop inside of a function

--- eak.js
+++ eak.js
@@ -1,12 +1,17 @@
 const squareCont = document.querySelector('.sContainer');
-const newSquare = document.createDocumentFragment();
 
 // Creates 256 divs in a 16x16 grid
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square'); // Add the 'square' class to each div
-    newSquare.appendChild(square);
+let makeSquares = () => {
+    const newSquare = document.createDocumentFragment(); // Create the fragment inside the function
+    for (let i = 0; i < 256; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square'); // Add the 'square' class to each div
+        newSquare.appendChild(square);
+    }
+
+    squareCont.appendChild(newSquare);
 }
 
-squareCont.appendChild(newSquare);
+// Call the function to create the squares
+makeSquares();
