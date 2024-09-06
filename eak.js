da83b0bf662ef8255fe927ce36b7e436b5dbda85
n-pontes <nuno06.np@gmail.com>
2024-09-06 10:47:27 +0200

    Correct the element selector in the dom tree - to .sContainer since its a class

--- eak.js
+++ eak.js
@@ -1,4 +1,4 @@
-const squareCont = document.querySelector('sContainer')
+const squareCont = document.querySelector('.sContainer')
 const square = document.createElement('div');
 square.style.width = '16px';
 square.style.height = '16px';
